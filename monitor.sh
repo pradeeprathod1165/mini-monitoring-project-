9a4d8013c14943603f46d21562ba0bc44c766437
pradeeprathod1165 <rishirathod1165@gmail.com>
2026-05-29 22:45:25 +0530

    monitor system script and development setup script

---
+++ monitor.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "=== System Info ===="
+uptime
+
+echo "==== Memory ===="
+free -h
+
+echo "=== Disk ==="
+df -h
+
+
---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo apt update
+
+sudo apt install -y \
+git \
+curl \
+wget \
+vim \
+htop \
+nginx
+
+echo "Development environment ready"
